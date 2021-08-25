253b59e671c5896ac55edc4045c77e19e92ba59b
jaducku <jaducku@kakao.com>
2021-08-25 20:15:31 +0800

    Type,Enum 추가

---
+++ typedefs-resolvers/_enums.js
@@ -0,0 +1,15 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    enum Role {
+        developer
+        designer
+        planner
+    }
+    enum NewOrUsed {
+        new
+        used
+    }
+`
+
+module.exports = typeDefs
--- typedefs-resolvers/equipments.js
+++ typedefs-resolvers/equipments.js
@@ -6,12 +6,36 @@ const typeDefs = gql`
         id: String
         used_by: String
         count: Int
-        new_or_used: String
+        new_or_used: NewOrUsed!
+    }
+    type EquipmentAdv {
+        id: ID!
+        used_by: Role!
+        count: Int!
+        use_rate: Float
+        is_new: Boolean
+        users: [String]
     }
 `
 const resolvers = {
     Query: {
         equipments: (parent, args) => dbWorks.getEquipments(args),
+        equipmentAdvs: (parent, args) => dbWorks.getEquipments(args)
+            .map((equipment) => {
+                if (equipment.used_by === 'developer') {
+                    equipment.use_rate = Math.random().toFixed(2)
+                }
+                equipment.is_new = equipment.new_or_uesd === 'new'
+                if (Math.random() > 0.5) {
+                    equipment.users = []
+                    dbWorks.getPeople(args).forEach((person) => {
+                        if (person.role === equipment.used_by && Math.random() < 0.2) {
+                            equipment.users.push(person.last_name)
+                        }
+                    })
+                }
+                return equipment
+            }),
     },
     Mutation: {
         deleteEquipment: (parent, args) => dbWorks.deleteItem('equipments', args),
