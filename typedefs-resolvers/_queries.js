454172b67a4e80dc30e3b2ced47fb45d6fa88c9c
Kyle <jaducku@kakao.com>
2021-08-27 12:25:38 +0000

    Person 추가

--- typedefs-resolvers/_queries.js
+++ typedefs-resolvers/_queries.js
@@ -10,6 +10,16 @@ const typeDefs = gql`
         givens: [Given]
         people: [People]
         person: People
+        peopleFiltered(
+            team: Int,
+            sex: Sex,
+            blood_type: BloodType,
+            from: String
+        ): [People]
+        peoplePaginated(
+            page: Int!,
+            per_page: Int!
+        ): [People]
     }
 `
 
